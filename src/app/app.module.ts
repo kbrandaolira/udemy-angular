41dc997bbc2197cf7236c08f97d56e7cc9625b9b
Lira, Kayan <kbrandaolira@gmail.com>
2018-07-08 12:03:49 -0300

    Importando Projeto Angular para o Github

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
+
+import {ROUTES} from './app.routes';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+
+
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './menu/menu.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [
+    RestaurantsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
